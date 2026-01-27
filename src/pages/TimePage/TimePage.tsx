e8a5c1d167e915700bdcec06ebcf8a933ca5b58d
Chenyang Ma <cma5@academic.rrc.ca>
2026-01-26 18:47:00 -0600

    feat: Add TimePage component with clockout integration

---
+++ src/pages/TimePage/TimePage.tsx
@@ -0,0 +1,12 @@
+import ClockInOut from "../../components/ClockIn-Out_alyssa/ClockInOut";
+
+function TimePage() {
+  return (
+    <section>
+      <h2>Time Tracking</h2>
+      <ClockInOut />
+    </section>
+  );
+}
+
+export default TimePage;
