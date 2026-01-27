003c6faf680a60c7bc9de38257bbe207e3d13916
Chenyang Ma <cma5@academic.rrc.ca>
2026-01-26 19:07:23 -0600

    feat:Update EmployeePage to receive and use shared state props

--- src/pages/EmployeePage/EmployeePage.tsx
+++ src/pages/EmployeePage/EmployeePage.tsx
@@ -1,9 +1,27 @@
+import { SharedStateProps } from "../../types";
 import EmployeeList from "../../components/component_chenyang/EmployeeList";
 
-function EmployeePage() {
+function EmployeePage({ currentUser, setCurrentUser }: SharedStateProps) {
   return (
     <section>
       <h2>Employee Directory</h2>
+      
+      {/* Display shared state */}
+      <p>Viewing as: {currentUser}</p>
+      
+      {/* Modify shared state */}
+      <div>
+        <label>Switch User: </label>
+        <select 
+          value={currentUser} 
+          onChange={(e) => setCurrentUser(e.target.value)}
+        >
+          <option value="Admin">Admin</option>
+          <option value="Manager">Manager</option>
+          <option value="Employee">Employee</option>
+        </select>
+      </div>
+
       <EmployeeList />
     </section>
   );
