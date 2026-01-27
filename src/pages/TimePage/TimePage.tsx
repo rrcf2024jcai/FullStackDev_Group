373ffc2e33e296bd5b69b4acfeeb3414126a3ee0
Chenyang Ma <cma5@academic.rrc.ca>
2026-01-26 19:08:40 -0600

    feat:Update TimePage to receive and use shared state props

--- src/pages/TimePage/TimePage.tsx
+++ src/pages/TimePage/TimePage.tsx
@@ -1,9 +1,27 @@
+import { SharedStateProps } from "../../types";
 import ClockInOut from "../../components/ClockIn-Out_alyssa/ClockInOut";
 
-function TimePage() {
+function TimePage({ currentUser, setCurrentUser }: SharedStateProps) {
   return (
     <section>
       <h2>Time Tracking</h2>
+      
+      {/* Display shared state */}
+      <p>Viewing as: {currentUser}</p>
+      
+      {/* Modify shared state */}
+      <div>
+        <label>Switch User: </label>
+        <select 
+          value={currentUser} 
+          onChange={(e) => setCurrentUser(e.target.value)}
+        >
+          <option value="Admin">Admin</option>
+          <option value="Manager">Manager</option>
+          <option value="Employee">Employee</option>
+        </select>
+      </div>
+
       <ClockInOut />
     </section>
   );
