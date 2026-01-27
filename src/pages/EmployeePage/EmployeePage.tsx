907c722c1a9b66d1eadea338804ca40499bd5d76
Chenyang Ma <cma5@academic.rrc.ca>
2026-01-26 18:39:42 -0600

    feat: Add EmployeePage component with EmloyeeList intergration.

---
+++ src/pages/EmployeePage/EmployeePage.tsx
@@ -0,0 +1,12 @@
+import EmployeeList from "../../components/component_chenyang/EmployeeList";
+
+function EmployeePage() {
+  return (
+    <section>
+      <h2>Employee Directory</h2>
+      <EmployeeList />
+    </section>
+  );
+}
+
+export default EmployeePage;
