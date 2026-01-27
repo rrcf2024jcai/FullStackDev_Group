5938452a716e103a5fda8d6e7d50ce503e35f0e9
Chenyang Ma <cma5@academic.rrc.ca>
2026-01-26 18:38:29 -0600

    feat: Add HonmePage component

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+      <section>
+        <h2>Welcome to PIXELL-River Financial Dashboard</h2>
+        <p>Select a section from the navigation to get started.</p>
+      </section>
+    );
+  }
+  
+  export default HomePage;
