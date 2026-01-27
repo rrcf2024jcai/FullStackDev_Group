533b305a05618750cfc48feb92e1926eb5ca1910
Chenyang Ma <cma5@academic.rrc.ca>
2026-01-26 18:48:45 -0600

    feat: Add LeavePage component with LeaveRequests integration.

---
+++ src/pages/LeavePage/LeavePage.tsx
@@ -0,0 +1,12 @@
+import LeaveRequests from "../../components/LeaveRequests_jiyu/LeaveRequests";
+
+function LeavePage() {
+  return (
+    <section>
+      <h2>Leave Management</h2>
+      <LeaveRequests />
+    </section>
+  );
+}
+
+export default LeavePage;
