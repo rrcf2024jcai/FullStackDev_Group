3aef725fc7acb37e4b2c8f7da42426fca98d8477
aldenurquiola-rrc <aldenurquiola@gmail.com>
2026-02-21 11:30:28 -0600

    T.3 Refactor shared-page state to hook service architecture and remove prop-drilling.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 // For Custom Hooks
 export { useEmployees } from "./useEmployees";
+export * from "./useCurrentUser";
+import { useCurrentUser } from "../hooks/useCurrentUser
---
+++ src/types/clock-in-out.ts
@@ -0,0 +1,9 @@
+/**
+ * Defines the structure for Clockin and Clock-Out.
+ */
+export interface ClockInOut {
+    id: number;
+    action: string;
+    time: string;
+    location: string;
+  }
