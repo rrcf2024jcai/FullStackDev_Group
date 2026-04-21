1c0474485f762366c3e2e9b87002b942232a29dc
rrcf2024jcai <jcai2@academic.rrc.ca>
2026-04-21 16:19:05 -0500

    Update main.tsx with the Provider

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,9 +1,18 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UserProvider } from "./context/userContext";
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <UserProvider>
-    <App />
-  </UserProvider>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </ClerkProvider>
 );
